9d15f1e84943caada190366fe209b0b6e405a322
Hamza S <hamza.samraoui@gmail.com>
2023-06-13 01:03:48 +0000

    13. Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,30 +1,17 @@
 #include "lists.h"
 
 /**
- * Definition for singly-linked list.
- * struct listint_s {
- * int n;
- * struct listint_s *next;
- * };
- * typedef struct listint_s listint_t;
+ * reverse_list - Reverses a linked list
+ * @head: Pointer to the head of the list
+ *
+ * Return: Pointer to the new head of the reversed list
  */
-
-int is_palindrome(listint_t **head)
+listint_t *reverse_list(listint_t **head)
 {
-  if (*head == NULL)
-    return 1;
-
-  listint_t *slow = *head;
-  listint_t *fast = *head;
-  while (fast != NULL && fast->next != NULL)
-    {
-      slow = slow->next;
-      fast = fast->next->next;
-    }
-
   listint_t *prev = NULL;
-  listint_t *current = slow;
-  listint_t *next;
+  listint_t *current = *head;
+  listint_t *next = NULL;
+
   while (current != NULL)
     {
       next = current->next;
@@ -33,15 +20,53 @@ int is_palindrome(listint_t **head)
       current = next;
     }
 
-  listint_t *left = *head;
-  listint_t *right = prev;
-  while (right != NULL)
+  return (prev);
+}
+
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome
+ * @head: Double pointer to the head of the list
+ *
+ * Return: 0 if not a palindrome, 1 if a palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+  listint_t *slow = *head;
+  listint_t *fast = *head;
+  listint_t *mid = NULL;
+  listint_t *second_half = NULL;
+
+  if (*head == NULL || (*head)->next == NULL)
+    return (1);
+
+  /* Find the middle node using slow and fast pointers */
+  while (fast != NULL && fast->next != NULL)
     {
-      if (left->n != right->n)
-	return 0;
-      left = left->next;
-      right = right->next;
+      slow = slow->next;
+      fast = fast->next->next;
     }
+
+  /* For odd number of nodes, skip the middle node */
+  if (fast != NULL)
+    {
+      mid = slow;
+      slow = slow->next;
+    }
+
+  /* Reverse the second half of the list */
+  second_half = reverse_list(&slow);
+
+  /* Compare the first and second halves of the list */
+  while (second_half != NULL)
+    {
+      if ((*head)->n != second_half->n)
+	return (0);
+      *head = (*head)->next;
+      second_half = second_half->next;
+    }
+
+  if (mid != NULL)
+    *head = (*head)->next;
 
   return (1);
 }
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,17 +1,20 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
 #include <stdlib.h>
 
-/* Definition for singly-linked list */
+/* Structure for singly-linked list */
 typedef struct listint_s
 {
   int n;
   struct listint_s *next;
 } listint_t;
 
-/* Function prototype */
+/* Function prototypes */
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+
 int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
