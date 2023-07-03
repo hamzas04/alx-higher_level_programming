83d0f5c14fa5645892ce1e09eab5a9a8f1eb4af0
Hamza S <hamza.samraoui@gmail.com>
2023-07-03 18:15:39 +0000

    2. Area and Perimeter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -2,45 +2,52 @@
 """ empty class Rectangle that defines a rectangle
 """
 
+
 class Rectangle:
-    """Class Rectangle"""
+    """ class rectangle"""
     def __init__(self, width=0, height=0):
-        """Instantiation with optional width and height"""
+        """ Instantiation with optional width and height"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Getter method for width"""
+        """ width
+        """
         return self.__width
+
+    @property
+    def height(self):
+        """ height
+        """
+        return self.__height
 
     @width.setter
     def width(self, value):
-        """Setter method for width"""
-        if not isinstance(value, int):
+        """ width setter
+        """
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-    @property
-    def height(self):
-        """Getter method for height"""
-        return self.__height
-
     @height.setter
     def height(self, value):
-        """Setter method for height"""
-        if not isinstance(value, int):
+        """ height setter
+        """
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
-        """Returns the rectangle area"""
+        """ returns rectangle area"""
         return self.__width * self.__height
 
     def perimeter(self):
-        """Returns the rectangle perimeter"""
-        return 2 * (self.__width + self.__height)
+        """ returns rectangle perimiter"""
+        if self.__width is 0 or self.__height is 0:
+            return 0
+        return self.__width * 2 + self.__height * 2
